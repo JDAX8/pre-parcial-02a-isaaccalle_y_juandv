199c5f183019244eefddc8776e4fec66dff85fb8
Isaac Calle <103228028+IsaakuTK@users.noreply.github.com>
2023-10-27 08:34:25 -0500

    adding basic structure and part of ducks

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+//instancias
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/public'))
+
+io.on( 'connection', (socket) => {
+    console.log('Un cliente se ha conectado');
+
+    socket.on ('enviar-elemento', (elemento)=>{
+        io.emit('elemento-recibido', elemento);
+    })
+
+    socket.on ('enviar-cursor', (elemento)=>{
+        io.emit('cursor-recibido', elemento);
+    })
+
+    socket.on ('cambiar-tamaño', (elemento)=>{
+        io.emit('tamaño-recibido', elemento);
+    })
+
+    socket.on ('cambiar-color', (elemento)=>{
+        io.emit('color-recibido', elemento);
+    })
+
+    io.on( 'disconnect', () => {
+        console.log('Un cliente se ha desconectado');
+    })
+});
+
+
+server.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+})
